aab3af7505342667c5cabc692cf8acf9f88d4796
mumei <64839630+ygoncloud@users.noreply.github.com>
2025-08-28 23:24:45 +0700

    feat: img fix carousel hidden

--- src/app/api/carousel/route.ts
+++ src/app/api/carousel/route.ts
@@ -7,7 +7,7 @@ export async function GET() {
   try {
     const carouselDirectory = path.join(process.cwd(), 'public', 'carousel');
     const filenames = fs.readdirSync(carouselDirectory);
-    const images = filenames.map((name) => `/carousel/${name}`);
+    const images = filenames.filter(name => !name.startsWith('.')).map((name) => `/carousel/${name}`);
     return NextResponse.json({ images });
   } catch (error) {
     return NextResponse.json({ error: 'Failed to read carousel directory' }, { status: 500 });
